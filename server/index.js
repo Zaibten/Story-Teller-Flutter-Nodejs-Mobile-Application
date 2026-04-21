9f4d970631b1cf730363810cc02c92007f7ecfd7
Muzamil Khan <113015136+Zaibten@users.noreply.github.com>
2026-04-21 20:26:13 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -796,12 +796,14 @@ app.get("/home", (req, res) => {
   `);
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running at:`);
-  console.log(`➡️ http://localhost:${PORT}`);
-  console.log(`➡️ http://0.0.0.0:${PORT}`);
-});
+// app.listen(PORT, "0.0.0.0", () => {
+//   console.log(`Server running at:`);
+//   console.log(`➡️ http://localhost:${PORT}`);
+//   console.log(`➡️ http://0.0.0.0:${PORT}`);
+// });
 
+
+export default app;
 
 
 
